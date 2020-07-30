44ecdd11b3cff448f9be41d9d48178486eb578a2
Julia Hildmann <julia_hildmann@web.de>
2020-07-30 19:23:03 +0200

    adding components for play and info game mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { PlayComponent } from './game/play/play.component';
+import { GameInfoComponent } from './game/game-info/game-info.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, GameComponent, PlayComponent, GameInfoComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
